1dd3eff6c2ed0911d19ed58dec899dafb469afc0
Tam Dang <dangitstam@Tams-MacBook-Pro.local>
2018-10-06 19:25:22 -0700

    baseline models

---
+++ library/dataset_readers/imdb_review_reader.py
@@ -0,0 +1,85 @@
+import logging
+from typing import Dict
+
+from allennlp.common.file_utils import cached_path
+from allennlp.common.util import END_SYMBOL, START_SYMBOL
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.fields import LabelField, TextField
+from allennlp.data.instance import Instance
+from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
+from allennlp.data.tokenizers import Tokenizer, WordTokenizer
+from allennlp.data.tokenizers.word_splitter import JustSpacesWordSplitter
+from overrides import overrides
+
+from .util import normalize_raw_text
+
+import ujson
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+@DatasetReader.register("imdb_review_reader")
+class IMDBReviewReader(DatasetReader):
+    """
+    Reads the 100K IMDB dataset in format that it appears in
+    http://ai.stanford.edu/~amaas/data/sentiment/
+    (i.e. this reader expects a full-path to the directory as a result of
+     extracting the tar).
+
+    This dataset reader will ensure the entire review is available to the model so that the
+    variational distribution is as accurate as possible. Unlike the above, training
+    is the goal.
+
+    Each ``read`` yields a data instance of
+        input_tokens: A backpropagation-through-time length portion of the review text as a ``TextField``
+        sentiment: Either `positive` or `negative`
+
+    Parameters
+    ----------
+    lazy : ``bool`` (optional, default=False)
+        Passed to ``DatasetReader``.  If this is ``True``, training will start sooner, but will
+        take longer per batch.  This also allows training with datasets that are too large to fit
+        in memory.
+    tokenizer : ``Tokenizer``, optional
+        Tokenizer to use to split text into English tokens.
+        Defaults to ``WordTokenizer()``.
+    token_indexers : ``Dict[str, TokenIndexer]``, optional
+        Indexers used to define English token representations. Defaults to ``{"tokens":
+        SingleIdTokenIndexer(namespace="en", lowercase_tokens=True)}``.
+    words_per_instance : ``int``, optional
+        The number of words in which the raw text will be bucketed to allow for more efficient
+        training (backpropagation-through-time limit).
+    """
+    def __init__(self,
+                 lazy: bool = False,
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                ) -> None:
+        super().__init__(lazy)
+        self._tokenizer = tokenizer or WordTokenizer()
+        self._token_indexers = token_indexers or {
+            "tokens": SingleIdTokenIndexer(namespace="tokens", lowercase_tokens=True)
+        }
+
+
+    @overrides
+    def _read(self, file_path):
+        # A training instance consists of the word frequencies for the entire review and a
+        # `words_per_instance`` portion of the review.
+        file_path = cached_path(file_path)
+        with open(cached_path(file_path), 'r') as data_file:
+            logger.info("Reading instances from lines in file: %s", file_path)
+            for line in data_file:
+                line = line.strip("\n")
+                if not line:
+                    continue
+                example = ujson.loads(line)
+                example_text = normalize_raw_text(example['text'])
+                example_text_tokenized = self._tokenizer.tokenize(example_text)
+                example_sentiment = "positive" if example['sentiment'] >= 5 else "negative"
+                example_instance = {
+                    'input_tokens': TextField(example_text_tokenized, self._token_indexers),
+                    'sentiment': LabelField(example_sentiment)
+                }
+
+                yield Instance(example_instance)
---
+++ library/models/seq2vec_classifier.py
@@ -0,0 +1,179 @@
+from collections import Counter
+from typing import Dict, Optional
+
+import torch
+import torch.nn as nn
+from allennlp.data.vocabulary import (DEFAULT_OOV_TOKEN, DEFAULT_PADDING_TOKEN,
+                                      Vocabulary)
+from allennlp.models.archival import load_archive
+from allennlp.models.model import Model
+from allennlp.modules import FeedForward, Seq2VecEncoder, TextFieldEmbedder
+from allennlp.nn import InitializerApplicator, RegularizerApplicator, util
+from allennlp.training.metrics import Average, CategoricalAccuracy
+from overrides import overrides
+from torch.distributions.multivariate_normal import MultivariateNormal
+from torch.nn.modules.linear import Linear
+
+from library.dataset_readers.util import STOP_WORDS
+
+from .util import compute_stopless_bow_vector, rnn_forward
+
+
+@Model.register("seq2vec_classifier")
+class Seq2VecClassifier(Model):
+    """ Simple generalized sequence encodding to classification.
+    """
+    def __init__(self, vocab: Vocabulary,
+                 encoder: Seq2VecEncoder,
+                 text_field_embedder: TextFieldEmbedder,
+                 initializer: InitializerApplicator = InitializerApplicator(),
+                 regularizer: Optional[RegularizerApplicator] = None) -> None:
+        super(Seq2VecClassifier, self).__init__(vocab, regularizer)
+
+        self.metrics = {
+            'accuracy': CategoricalAccuracy()
+        }
+
+        self.encoder = encoder
+        self.output_projection = Linear(self.encoder.get_output_dim(), 2)
+        self.text_field_embedder = text_field_embedder
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+        initializer(self)
+
+    @overrides
+    def forward(self, # pylint: disable=arguments-differ
+                input_tokens: Dict[str, torch.LongTensor],
+                sentiment: Dict[str, torch.LongTensor]):
+
+        output_dict = {}
+
+        # Encode the current input text, incorporating previous hidden state if available.
+        # Shape: (batch x BPTT limit x hidden size)
+        embedded_input = self.text_field_embedder(input_tokens)
+        input_mask = util.get_text_field_mask(input_tokens)
+        encoded_input = self.encoder(embedded_input, input_mask)
+
+        # Compute logits.
+        # Shape: (batch x num_classes)
+        logits = self.output_projection(encoded_input)
+
+        # Loss and metrics.
+        output_dict['loss'] = self.criterion(logits, sentiment)
+        self.metrics['accuracy'](logits, sentiment)
+
+        return output_dict
+
+    @overrides
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}
+
+
+@Model.register("seq2vec_vae_classifier")
+class Seq2VecClassifierVAE(Model):
+    """ Generalized sequence encodding to classification supplemented with a VAE
+        that reconstructs the bag-of-words represnetation.
+    """
+    def __init__(self, vocab: Vocabulary,
+                 encoder: Seq2VecEncoder,
+                 text_field_embedder: TextFieldEmbedder,
+                 latent_dim: int = 512,
+                 initializer: InitializerApplicator = InitializerApplicator(),
+                 regularizer: Optional[RegularizerApplicator] = None) -> None:
+        super(Seq2VecClassifierVAE, self).__init__(vocab, regularizer)
+
+        self.metrics = {
+            'KL-Divergence': Average(),
+            'Accuracy': CategoricalAccuracy()
+        }
+
+        self.vocab = vocab
+        self.encoder = encoder
+        self.text_field_embedder = text_field_embedder
+        self.latent_dim = latent_dim
+        self.classification_criterion = torch.nn.CrossEntropyLoss()
+        self.reconstruction_criterion = torch.nn.MSELoss()
+
+        # Produce the namespace that excludes stopwords.
+        self.stopless_namespace = "stopless"
+        for word, _ in self.vocab.get_token_to_index_vocabulary().items():
+            if word not in STOP_WORDS:
+                self.vocab.add_token_to_namespace(word, self.stopless_namespace)
+        self.stopless_dim = self.vocab.get_vocab_size(self.stopless_namespace)
+
+        feature_dim = self.encoder.get_output_dim() + self.latent_dim
+        self.output_projection = Linear(feature_dim, 2)
+
+        # The VAE first projections word-frequencies via a feedforward network g(.).
+        # Mean mu is computed via W_mu(g(.)) + a_mu.
+        # Sigma is computed as W_sigma(g(.)) + a_sigma.
+        self.initial_latent_projection = FeedForward(
+            self.stopless_dim,
+            1,
+            [latent_dim * 2],
+            [torch.nn.Tanh()]
+        )
+
+        self.mu_projection = Linear(latent_dim * 2, latent_dim)
+        self.sigma_projection = Linear(latent_dim * 2, latent_dim)
+        self.reconstruction_projection = Linear(latent_dim, self.stopless_dim)
+
+        # Noise used to implement the reparameterization trick.
+        # Prior will be a 0, 1 multivariate gaussian.
+        self.noise = MultivariateNormal(torch.zeros(latent_dim), torch.eye(self.latent_dim))
+
+        initializer(self)
+
+    @overrides
+    def forward(self, # pylint: disable=arguments-differ
+                input_tokens: Dict[str, torch.LongTensor],
+                sentiment: Dict[str, torch.LongTensor]):
+
+        output_dict = {}
+
+        # Encode the current input text, incorporating previous hidden state if available.
+        # Shape: (batch x BPTT limit x hidden size)
+        embedded_input = self.text_field_embedder(input_tokens)
+        input_mask = util.get_text_field_mask(input_tokens)
+        encoded_input = self.encoder(embedded_input, input_mask)
+
+        # Bag-of-words representation.
+        # TODO: init latent proj needs to know the stopless dim...
+        stopless_bow = compute_stopless_bow_vector(self.vocab, input_tokens, self.stopless_namespace)
+
+        # Compute the variational distribution.
+        init_latent_bow = self.initial_latent_projection(stopless_bow)
+        mu = self.mu_projection(init_latent_bow)
+        log_sigma_squared = self.sigma_projection(init_latent_bow)
+
+        # Sample from the VAE.
+        epsilon = self.noise.rsample(sample_shape=torch.Size([mu.size(0)]))
+        latent_bow = mu + torch.sqrt(torch.exp(log_sigma_squared)) * epsilon
+
+        features = torch.cat([encoded_input, latent_bow], dim=-1)
+
+        # Train the VAE to make the latent features rich.
+        reconstructed_bow = self.reconstruction_projection(latent_bow)
+        reconstruction_loss = self.reconstruction_criterion(stopless_bow, reconstructed_bow)
+
+        # Compute logits.
+        # Shape: (batch x num_classes)
+        logits = self.output_projection(features)
+        classification_loss = self.classification_criterion(logits, sentiment)
+
+        # Loss and metrics.
+        negative_kl_divergence = torch.sum(log_sigma_squared - mu ** 2 - torch.exp(log_sigma_squared), dim=-1)
+        negative_kl_divergence += torch.ones(mu.size(0))
+        negative_kl_divergence = negative_kl_divergence.sum() / 2
+
+        # Joint learning of classification and the VAE.
+        # Negative KL-div needs to be negated since loss is negative likelihood.
+        output_dict['loss'] = -negative_kl_divergence + reconstruction_loss + classification_loss
+        self.metrics['KL-Divergence'](-negative_kl_divergence)
+        self.metrics['Accuracy'](logits, sentiment)
+
+        return output_dict
+
+    @overrides
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}
