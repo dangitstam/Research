231d90555c25261ec58465051328371d8520113c
Tam Dang <dangitstam@D-10-19-52-156.dhcp4.washington.edu>
2018-10-09 10:45:57 -0700

    wip

--- library/models/unsupervised.py
+++ library/models/unsupervised.py
@@ -1,40 +1,52 @@
 import json
-from collections import Counter
 from typing import Dict, Optional
 
 import torch
-import torch.nn as nn
-from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.nn.modules.linear import Linear
 
-from allennlp.data.vocabulary import (DEFAULT_OOV_TOKEN, DEFAULT_PADDING_TOKEN,
-                                      Vocabulary)
+from allennlp.data.vocabulary import Vocabulary
 from allennlp.models.archival import load_archive
 from allennlp.models.model import Model
-from allennlp.modules import FeedForward, Seq2VecEncoder, TextFieldEmbedder
+from allennlp.modules import Seq2VecEncoder, TextFieldEmbedder
 from allennlp.nn import InitializerApplicator, RegularizerApplicator, util
 from allennlp.training.metrics import Average, CategoricalAccuracy
 from overrides import overrides
 
-from library.dataset_readers.util import STOP_WORDS
+from library.dataset_readers.util import STOP_WORDS  # TODO: Explore stopless version.
 from library.models.vae import VAE
 
 from .util import (compute_bow_vector, log_standard_categorical,
                    sort_unsupervised_instances)
 
-# TODO: Explore stopless version?
 
 @Model.register("BOWSeq2VecClassifier")
 class BOWSeq2VecClassifier(Model):
-    """ Generalized sequence encoding to classification supplemented with a VAE
-        that reconstructs the bag-of-words represnetation.
+    """
+    Joint topic model and text classifier, training the VAE in a semi-supervised
+    environment (https://arxiv.org/abs/1406.5298).
+
+    Parameters
+    ----------
+    vocab : ``Vocabulary``, required
+        A Vocabulary, required in order to compute sizes for input/output projections.
+    encoder : ``Seq2VecEncoder``
+        The encoder used to encode input text.
+    text_field_embedder : ``TextFieldEmbedder``, required
+        Used to embed the ``tokens`` ``TextField`` we get as input to the model.
+    vae : ``VAE``
+        The variational autoencoder used to project the BoW into a latent space.
+    precompued_word_counts: ``str``
+        Path to a JSON file containing word counts accumulated over the training corpus.
+    initializer : ``InitializerApplicator``, optional (default=``InitializerApplicator()``)
+        Used to initialize the model parameters.
+    regularizer : ``RegularizerApplicator``, optional (default=``None``)
+        If provided, will be used to calculate the regularization penalty during training.
     """
     def __init__(self, vocab: Vocabulary,
                  encoder: Seq2VecEncoder,
                  vae: VAE,
                  text_field_embedder: TextFieldEmbedder,
-                 precomputed_word_counts: str,
-                 latent_dim: int = 512,
+                 precomputed_word_counts: str = None,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super(BOWSeq2VecClassifier, self).__init__(vocab, regularizer)
@@ -49,21 +61,15 @@ class BOWSeq2VecClassifier(Model):
         self.vae = vae
         self.text_field_embedder = text_field_embedder
 
-        # Load in the word counts from the JSON file and compute the background
-        # log term frequency.
-        precomputed_word_counts = json.load(open(precomputed_word_counts, "r"))
-        self.log_term_frequency = torch.FloatTensor(self.vocab.get_vocab_size())
-        for i in range(self.vocab.get_vocab_size()):
-            token = self.vocab.get_token_from_index(i)
-            if token in precomputed_word_counts:
-                self.log_term_frequency[i] = precomputed_word_counts[token]
-        self.log_term_frequency = torch.softmax(self.log_term_frequency, -1)
-
-        self.latent_dim = latent_dim
+        self.latent_dim = vae.encoder.get_output_dim()
         self.classification_criterion = torch.nn.CrossEntropyLoss()
         self.reconstruction_criterion = torch.nn.MSELoss()
 
-        feature_dim = self.encoder.get_output_dim() + latent_dim
+        # Background log frequency vector.
+        self.log_term_frequency = self._compute_background_log_frequency(precomputed_word_counts)
+
+        # Note that the VAE's encoder is the initial projection into the latent space.
+        feature_dim = self.encoder.get_output_dim() + self.vae.encoder.get_output_dim()
         self.output_projection = Linear(feature_dim, 2)
 
         # The latent topics learned.
@@ -103,14 +109,21 @@ class BOWSeq2VecClassifier(Model):
                            input_tokens: torch.Tensor,
                            sentiment: torch.Tensor):
         """
-        Supervised portion of training loss: KL-Divergence, reconstruction, and cross entropy against the labels.
+        Computes loss for labelled data.
+
+        :param input_tokens: ``torch.Tensor``
+            The tokenized input, expected as (batch, sequence length)
+        :param sentiment: ``torch.Tensor``
+            The target class labels, expexted as (batch,)
+
+        Returns the sum of ELBO and the entropy of the predicted classification logits.
         """
-        
         if input_tokens.size(0) == 0:
             return 0, 0
 
         elbo, logits = self.ELBO(input_tokens)
 
+        # TODO: Alpha parameter on classification loss.
         # Supplementary classification loss.
         classification_loss = self.classification_criterion(logits, sentiment)
         self.metrics['Accuracy'](logits, sentiment)
@@ -119,15 +132,19 @@ class BOWSeq2VecClassifier(Model):
 
     def unsupervised_forward(self, input_tokens: torch.Tensor): # pylint: disable=C0103
         """
-        Supervised portion of training loss: KL-Divergence, reconstruction, and cross entropy against the labels.
-        """
+        Computes loss for unlabelled data.
 
+        :param input_tokens: ``torch.Tensor``
+            The tokenized input, expected as (batch, sequence length)
+
+        Returns the sum of ELBO and the entropy of the predicted classification logits.
+        """
         if input_tokens.size(0) == 0:
             return 0
 
         elbo, logits = self.ELBO(input_tokens)
 
-        # Calculate entropy H(q(y|x)) and sum over all labels.
+        # Compute q(y | x)(-ELBO) and entropy H(q(y|x)), sum over all labels.
         H = -torch.sum(logits * torch.log(logits + 1e-8), dim=-1) # pylint: disable=C0103
         L = torch.sum(logits * elbo, dim=-1) # pylint: disable=C0103
 
@@ -135,10 +152,13 @@ class BOWSeq2VecClassifier(Model):
 
     def ELBO(self, input_tokens: torch.Tensor):  # pylint: disable=C0103
         """
-        ELBO loss: KL-Divergence, reconstruction, and cross entropy against the labels assuming
-        a uniform prior.
+        Computes ELBO loss:
+        KL-Divergence, reconstruction, and log likelihood of the labels assuming a uniform prior.
 
-        Returns both ELBO and the classification logits.
+        :param input_tokens: ``torch.Tensor``
+            The tokenized input, expected as (batch, sequence length)
+
+        Returns both ELBO and the classification logits for convenience.
         """
 
         # Encode the current input text, incorporating previous hidden state if available.
@@ -167,17 +187,28 @@ class BOWSeq2VecClassifier(Model):
         # We assume a uniform prior for y.
         label_prior = -log_standard_categorical(logits)
 
-        # Loss and metrics.
         negative_kl_divergence = 1 + torch.log(sigma ** 2) - mu ** 2 - sigma ** 2
         negative_kl_divergence = 0.5 * negative_kl_divergence.sum()
-
         self.kl_divergence += -negative_kl_divergence.item()
 
         # Joint learning of classification and the VAE for labeled instances.
-        # elbo = E[P(x | z, y) + P(y)] - KL-Div(q(z | x, y), P(z))
+        # ELBO = E[P(x | z, y) + P(y)] - KL-Div(q(z | x, y), P(z))
         elbo = -reconstruction_loss + label_prior + negative_kl_divergence
 
         return elbo, logits
+
+    def _compute_background_log_frequency(self, precomputed_word_counts: str):
+        """ Load in the word counts from the JSON file and compute the
+            background log term frequency w.r.t this vocabulary. """
+        precomputed_word_counts = json.load(open(precomputed_word_counts, "r"))
+        log_term_frequency = torch.FloatTensor(self.vocab.get_vocab_size())
+        for i in range(self.vocab.get_vocab_size()):
+            token = self.vocab.get_token_from_index(i)
+            if token in precomputed_word_counts:
+                self.log_term_frequency[i] = precomputed_word_counts[token]
+        log_term_frequency = torch.softmax(self.log_term_frequency, -1)
+
+        return log_term_frequency
 
     @overrides
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
